1e4caf8444d349645431e11e0a2dfd843e6ae52b
harvey67 <67565077+harvey67@users.noreply.github.com>
2020-10-14 09:58:20 +1100

    Updates

---
+++ precache-manifest.dd420ddeee718827bb9ec845a1f272c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732bf44eaab6fea89bf5df1e694ca5a0",
+    "url": "/lyrics/index.html"
+  },
+  {
+    "revision": "dde37c1963e84f15971d",
+    "url": "/lyrics/static/css/main.e24e73b2.chunk.css"
+  },
+  {
+    "revision": "63e7160c84bcbc1af780",
+    "url": "/lyrics/static/js/2.e5acc750.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lyrics/static/js/2.e5acc750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dde37c1963e84f15971d",
+    "url": "/lyrics/static/js/main.b97177b9.chunk.js"
+  },
+  {
+    "revision": "63ac4f358dd881491e2c",
+    "url": "/lyrics/static/js/runtime-main.cfabec72.js"
+  }
+]);
